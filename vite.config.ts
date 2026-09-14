e72f7e2dc32f10a51f9ef70f3d3be700992f5cbc
Hana <enuxura@gmail.com>
2026-09-14 23:21:20 +0200

    Test fix `disable MIME type` error

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+	plugins: [svelte()],
+	base: "https://hanako-eo.github.io/indie-card-maker/",
 })
